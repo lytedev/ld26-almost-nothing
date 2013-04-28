4d0270fd5c126d4652a253233c899b263749c410
Daniel Flanagan <wraithx2@gmail.com>
2013-04-28 03:59:23 -0500

    1.0 - Release 1 - Fixed Zip Hierarchy and Packager

--- package.bat
+++ package.bat
@@ -7,12 +7,15 @@ cp C:\Users\Public\Applications\Love2d\love.exe ./love.exe
 :: copy /b love.exe+almost-nothing.love almost-nothing.exe
 :: 7z a almost-nothing.zip almost-nothing.exe
 echo love.exe almost-nothing.love >> play.bat
-7z a almost-nothing.zip play.bat
-7z a almost-nothing.zip love.exe
-7z a almost-nothing.zip almost-nothing.love
-7z a almost-nothing.zip C:\Users\Public\Applications\Love2d\DevIL.dll
-7z a almost-nothing.zip C:\Users\Public\Applications\Love2d\OpenAL32.dll
-7z a almost-nothing.zip C:\Users\Public\Applications\Love2d\SDL.dll
+mkdir almost-nothing
+cp almost-nothing.love almost-nothing/almost-nothing.love
+cp play.bat almost-nothing/play.bat
+cp love.exe almost-nothing/love.exe
+cp C:\Users\Public\Applications\Love2d\DevIL.dll almost-nothing/DevIL.dll
+cp C:\Users\Public\Applications\Love2d\OpenAL32.dll almost-nothing/OpenAL32.dll
+cp C:\Users\Public\Applications\Love2d\SDL.dll almost-nothing/SDL.dll
+cp play.bat almost-nothing/play.bat
+7z a almost-nothing.zip almost-nothing
 del love.exe
 del play.bat
 mv ../ld26.git ./.git
